dee51e71db1e4e1f3b552010c1818392c9f729b2
Abbie Kressner <abbiekressner@gmail.com>
2011-10-31 10:02:21 -0500

    Make the inputs to prettyBarPlot easier.

--- prettyBarPlot.m
+++ prettyBarPlot.m
@@ -1,29 +1,37 @@
-function handle=prettyBarPlot(vals,names,axislabel,confInt,handleToBarPlotFunc)
+function handle=prettyBarPlot(vals,names,axislabel,confIntWidths,direction)
 
-	% handle=prettyBarPlot(vals,names,axislabel,confInt,handleToBarPlotFunc)
+	% handle=prettyBarPlot(vals,names,axislabel,confIntWidths,handleToBarPlotFunc)
 	%
 	% Inputs-
 	%	vals				= vector containing the correlation values you want to plot
 	%	names				= cell array of strings containing names for each case
 	%	axislabel			= string for the axis label
-	%	confInt				= 2 column matrix containing lower and upper bounds; specify as false to skip
+	%	confIntWidths		= 2 column matrix containing widths of lower and upper bounds from the mean; specify as false to skip
 	%		CORRELATION ANALYSIS
 	%			confwidths=intervalEstimationOfCorrCoeff(vals,numberPerCondition); % 95% confidence by default
-	%			lower=vals(:)-confwidths(:,1);
-	%			upper=confwidths(:,2)-vals(:);
-	%			confInt=[lower(:) upper(:)];
 	%		STMI BONFERRONI
 	%			use the halfwidths from multcompare (see abbie_multcompare and/or statTesting)
-	%	handleToBarPlotFunc	= handle to the plotting function (defaults to @barh)
-	%						 call bar to plot vertical bars
-	%						 call barh to plot horizontal bars
+	%	direction			= plot the bars horizontally ('h') or vertically ('v')
 
 
 if nargin<5
+	direction='h';
+end
+if strcmp(direction,'v')
+	handleToBarPlotFunc=@bar;
+	handleToErrorBarFunc=@errorbar;
+	whichAxisContainsGroupNames='X';
+	whichAxisContainsTheValues='Y';
+	handleToLabelFunction=@ylabel;
+else
 	handleToBarPlotFunc=@barh;
+	handleToErrorBarFunc=@errorbar_x;
+	whichAxisContainsGroupNames='Y';
+	whichAxisContainsTheValues='X';
+	handleToLabelFunction=@xlabel;
 end
 if nargin<4
-	confInt=false;
+	confIntWidths=false;
 end
 if nargin<3
 	axislabel='|r_p|';
@@ -40,13 +48,13 @@ handle=handleToBarPlotFunc(vals,0.7,'FaceColor',mainBarColor*[1 1 1],'Edgecolor'
 
 set(gca,...
 	'TickDir','out',...
-	'XGrid','on',...
-	'YLim',[0.5 length(vals)+0.5],...
-	'YLimMode','manual',...
-	'YTickMode','manual',...
-	'YTick',1:length(vals),...
-	'YTickLabelMode','manual',...
-	'YTickLabel',names,...
+	[whichAxisContainsTheValues 'Grid'],'on',...
+	[whichAxisContainsGroupNames 'Lim'],[0.5 length(vals)+0.5],...
+	[whichAxisContainsGroupNames 'LimMode'],'manual',...
+	[whichAxisContainsGroupNames 'TickMode'],'manual',...
+	[whichAxisContainsGroupNames 'Tick'],1:length(vals),...
+	[whichAxisContainsGroupNames 'TickLabelMode'],'manual',...
+	[whichAxisContainsGroupNames 'TickLabel'],names,...
 	'FontSize',fontSize,...
 	'FontName','Helvetica Neue',...
 	'FontWeight','normal',...
@@ -54,14 +62,20 @@ set(gca,...
 	'Box','off');
 set(handle,'ShowBaseLine','off',...
 	'LineWidth',1.2);
-xlabel(['$' axislabel '$'],...
+handleToLabelFunction(['$' axislabel '$'],...
 	'interpreter','latex',...
 	'FontSize',30);
 
 % Add confidence intervals
-if not(islogical(confInt))
-	errorHandle=errorbar_x(vals(:).',1:length(vals),...
-		confInt(:,1),confInt(:,2),'k-');
+if not(islogical(confIntWidths))
+	if strcmp(direction,'v')
+		xandy=[(1:length(vals)).' vals(:)];
+	else
+		xandy=[vals(:) (1:length(vals)).'];
+	end
+	
+	errorHandle=handleToErrorBarFunc(xandy(:,1),xandy(:,2),...
+		confIntWidths(:,1),confIntWidths(:,2),'k-'); % NOTE this 'k-' is causing problems with the vertical case
 	set(errorHandle,...
 		'LineWidth',1.75,...
 		'Color',accentColor*[1 1 1]);
@@ -69,13 +83,17 @@ end
 
 % Add text labels of the values
 for ii=1:length(vals)
-	if exist('confInt','var')
-		xPosForText=confInt(ii,2);
+	if exist('confIntWidths','var')
+		whereToPutValue=vals(ii)+confIntWidths(ii,2);
 	else
-		xPosForText=vals(ii);
+		whereToPutValue=vals(ii);
 	end
-	defaultXaxisLength=diff(get(gca,'XLim'));
-	text(xPosForText+0.02*defaultXaxisLength,ii,...
+	if strcmp(direction,'v')
+		xandy=[ii,whereToPutValue+0.02*diff(get(gca,'YLim'))];
+	else
+		xandy=[whereToPutValue+0.02*diff(get(gca,'XLim')),ii];
+	end
+	text(xandy(1),xandy(2),...
 		sprintf('%1.2f',vals(ii)),...
 		'FontSize',fontSize,...
 		'FontName','Helvetica Neue',...
