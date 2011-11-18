a90d76119bae81967956ea0a906336a2e058bf4c
Abbie Kressner <abbiekressner@gmail.com>
2011-11-18 16:34:38 -0500

    New minAndMax function.

---
+++ minAndMax.m
@@ -0,0 +1,8 @@
+function out=minAndMax(x)
+
+	% FUNCTION out=minAndMax(x)
+	%
+	% out is a 2 element vector with this minimum and maximum, respective,
+	% values of the maxtrix, x.
+
+	out=[min(x(:)) max(x(:))];
