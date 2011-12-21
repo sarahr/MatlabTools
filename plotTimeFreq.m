c57d5efcf97426f21ec0cdcec37157d422b1ad21
Abbie Kressner <abbiekressner@gmail.com>
2011-12-21 13:03:38 -0500

    Fix order of time and freq in plotTimeFreq.

--- plotTimeFreq.m
+++ plotTimeFreq.m
@@ -1,13 +1,19 @@
 function plotTimeFreq(theFillInTheBlankGram,freqs)
 
+	 % FUNCTION plotTimeFreq(theFillInTheBlankGram,freqs)
+	 %
+	 % theFillInTheBlankGram:	time by freq (i.e. time along first dim)
+	 % freqs:					vector of freqs
+	 %								length=size(theFillInTheBlankGram,2)
+
 secondsPerFrame=0.008;  % Assumption
 fMarkers=[250 500 1000 2000 4000];
 
-N_timeFrames=size(theFillInTheBlankGram,2);
-N_freqChannels=size(theFillInTheBlankGram,1);
+N_timeFrames=size(theFillInTheBlankGram,1);
+N_freqChannels=size(theFillInTheBlankGram,2);
 time=linspacestep(0,secondsPerFrame,N_timeFrames);
 
-imagesc(theFillInTheBlankGram); 
+imagesc(theFillInTheBlankGram.'); 
 axis('xy');
 % caxis([-90 -30]);  % not sure what to do with this yet TODO
 
